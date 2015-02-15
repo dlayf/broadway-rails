57c28d0df02718eeb558c79cab8b0968fc34f35f
dlayf <dan.layfield@gmail.com>
2015-02-15 14:57:12 -0500

    added home action and html and css

--- broadway/config/routes.rb
+++ broadway/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "url/pages/home" => 'pages#home'
+
+  root 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
